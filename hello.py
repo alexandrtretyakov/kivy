2abf0c33a6155fed1b19c8e69dc9b01e8dfb05c2
Alexandr Tretyakov <alex@tretyakov.net>
2021-08-09 10:00:09 +0300

    Закончил прогу

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 
 class MainWidget(BoxLayout):
-    pass
+    hello_label = ObjectProperty()
+    name_input = ObjectProperty()
+
+    def say_hello(self):
+        self.hello_label.text = 'Привет ' + self.name_input.text
 
 
 class MainApp(App):
